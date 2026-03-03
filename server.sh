0ff1d1084961b78642ddc90810500f4ab6947ddb
Elliott Weston Ball <elliottwball@gmail.com>
2026-03-03 10:38:40 -0800

    added same logic for server start

---
+++ server.sh
@@ -0,0 +1,4 @@
+# the python script to commit the changes to git, but for linux users :)
+# ik its a bit redundant but it works, and its nicer to just type "./server.sh" instead of "python3 server-start.py"
+
+python3 server-start.py
