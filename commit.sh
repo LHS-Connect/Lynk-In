5d3217d8ec4ebd62604b3b056f05292877082323
Elliott Weston Ball <elliottwball@gmail.com>
2026-03-03 16:41:09 -0800

    wow

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 # the python script to commit the changes to git, but for linux users :)
-# ik its a bit redundant but it works, and its nicer to just type "./commit.sh" instead of "python3 commit.py"
+# ik its a bit redundant but it works, and its nicer to just type "./commit.sh" instead of "python3 commit.py" :)
 
 python3 commit.py
