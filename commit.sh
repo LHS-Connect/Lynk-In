3fec228aea75421f6e4dad468bc94b276b58e715
Elliott Weston Ball <elliottwball@gmail.com>
2026-03-04 10:03:30 -0800

    Changed :: commit.sh comments | Reason :: dont want to leave out mac users :)

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
-# the python script to commit the changes to git, but for linux users :)
+# the python script to commit the changes to git, but for linux/mac users :)
 # ik its a bit redundant but it works, and its nicer to just type "./commit.sh" instead of "python3 commit.py" :)
 
 python3 commit.py
